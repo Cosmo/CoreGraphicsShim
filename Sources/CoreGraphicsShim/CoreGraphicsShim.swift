07f02770b70cc443200a8e856712c57f07adbea7
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-12-22 18:43:47 +0100

    Update CoreGraphicsShim.swift

--- Sources/CoreGraphicsShim/CoreGraphicsShim.swift
+++ Sources/CoreGraphicsShim/CoreGraphicsShim.swift
@@ -4,27 +4,27 @@ import Foundation
 // import CoreGraphics
 #endif
 
-enum CGLineCap: Int32 {
+public enum CGLineCap: Int32 {
     case butt = 0
     case round = 1
     case square = 2
 }
 
-enum CGLineJoin: Int32 {
+public enum CGLineJoin: Int32 {
     case miter = 0
     case round = 1
     case bevel = 2
 }
 
-class CGPath {
+public class CGPath {
 }
 
-class CGMutablePath: CGPath {
+public class CGMutablePath: CGPath {
     
 }
 
-struct CGAffineTransform {
-    static var identity: CGAffineTransform {
+public struct CGAffineTransform {
+    public static var identity: CGAffineTransform {
         return CGAffineTransform()
     }
 }
