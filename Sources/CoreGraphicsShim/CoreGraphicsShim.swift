bb5785fc0424e2876a1449449148c9e3308ad273
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-12-22 03:07:05 +0100

    Add initial shims

--- Sources/CoreGraphicsShim/CoreGraphicsShim.swift
+++ Sources/CoreGraphicsShim/CoreGraphicsShim.swift
@@ -1,3 +1,30 @@
-struct CoreGraphicsShim {
-    var text = "Hello, World!"
+#if os(Linux)
+import Foundation
+#else
+// import CoreGraphics
+#endif
+
+enum CGLineCap: Int32 {
+    case butt = 0
+    case round = 1
+    case square = 2
+}
+
+enum CGLineJoin: Int32 {
+    case miter = 0
+    case round = 1
+    case bevel = 2
+}
+
+class CGPath {
+}
+
+class CGMutablePath: CGPath {
+    
+}
+
+struct CGAffineTransform {
+    static var identity: CGAffineTransform {
+        return CGAffineTransform()
+    }
 }
